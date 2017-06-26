7ecb4b579b1f6eca64c26ccd394a75277e558667
Filippo Squillace <squillace.filippo@gmail.com>
2017-06-26 15:17:14 +0200

    First commit!

---
+++ utils.sh
@@ -0,0 +1,41 @@
+unset HOME PEARL_ROOT PEARL_HOME
+
+function pearlSetUp(){
+    pearlRootSetUp
+    pearlHomeSetUp
+}
+
+function pearlTearDown(){
+    pearlRootTearDown
+    pearlHomeTearDown
+}
+
+function pearlHomeSetUp(){
+    HOME=$(TMPDIR=/tmp mktemp -d -t pearl-user-home.XXXXXXX)
+    mkdir -p $HOME
+    export PEARL_HOME=${HOME}/.config/pearl
+    mkdir -p $PEARL_HOME
+    mkdir -p $PEARL_HOME/bin
+    mkdir -p $PEARL_HOME/etc
+    touch $PEARL_HOME/etc/pearl.conf
+    mkdir -p $PEARL_HOME/repos
+    mkdir -p $PEARL_HOME/packages
+    mkdir -p $PEARL_HOME/tmp
+}
+
+function pearlHomeTearDown(){
+    rm -rf $PEARL_HOME
+    rm -rf $HOME
+    unset PEARL_HOME
+}
+
+# The Pearl setup is useful when the package requires
+# library dependencies such as $PEARL_ROOT/lib/utils/utils.sh
+function pearlRootSetUp() {
+    export PEARL_ROOT=$(TMPDIR=/tmp mktemp -d -t pearl-test-root.XXXXXXX)
+}
+
+function pearlRootTearDown(){
+    rm -rf $PEARL_ROOT
+    unset PEARL_ROOT
+}
