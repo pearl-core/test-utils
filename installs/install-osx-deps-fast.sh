9f120a5315d010a86161c1a479c0f08ef8e7e4a3
Filippo Squillace <squillace.filippo@gmail.com>
2019-05-26 15:56:15 +1000

    Revert "Add coreutils, gnu-sed and grep brew packages"
    This reverts commit ca6ee0d9853306630f8f9c069198429fdc04418e.

--- installs/install-osx-deps-fast.sh
+++ installs/install-osx-deps-fast.sh
@@ -3,6 +3,8 @@
 set -ex
 
 brew update
-brew install bash zsh git coreutils gnu-sed grep
+brew install bash zsh
+# Coreutils and git should be already installed on OSX 7.3+ images:
+#brew install coreutils git
 
 ./tests/test-utils/installs/install-fish.sh "$TRAVIS_FISH_VERSION"
--- installs/install-osx-deps.sh
+++ installs/install-osx-deps.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 set -ex
 brew update
-brew install coreutils gnu-sed grep
+# Coreutils and git should be already installed on OSX 7.3+ images:
+#brew install coreutils git
 # Openssl is required for installing git from source code
 # Findutils is required for `fonts` Pearl package
 brew install openssl findutils
