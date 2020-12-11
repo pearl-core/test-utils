88b16f57b94fd650a8c19eb485bd313c4b53e3d2
Filippo Squillace <squillace.filippo@gmail.com>
2020-12-11 14:01:51 +0100

    Fix osx scripts

--- installs/install-osx-deps-fast.sh
+++ installs/install-osx-deps-fast.sh
@@ -2,6 +2,8 @@
 
 set -ex
 
+BASE_NAME="$(dirname $0)"
+
 # There is an intermittent issue with DNS resolution in OSX with Travis.
 # Fixing it with a static ip:
 STATIC_IP="$(dig +short +tcp github.com)"
@@ -9,10 +11,8 @@ sudo bash -c "echo '${STATIC_IP} github.com' >> /private/etc/hosts"
 cat /private/etc/hosts
 
 brew update
-./tests/test-utils/installs/brew-install bash zsh grep gnu-sed
+"${BASE_NAME}"/brew-install bash zsh grep gnu-sed
 # Coreutils and git should be already installed on OSX 7.3+ images:
 #brew install coreutils git
-
-BASE_NAME="$(dirname $0)"
 
 "${BASE_NAME}"/install-fish.sh "$TRAVIS_FISH_VERSION"
--- installs/install-osx-deps.sh
+++ installs/install-osx-deps.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -ex
 
+BASE_NAME="$(dirname $0)"
+
 # There is an intermittent issue with DNS resolution in OSX with Travis.
 # Fixing it with a static ip:
 STATIC_IP="$(dig +short +tcp github.com)"
@@ -8,14 +10,12 @@ sudo bash -c "echo '${STATIC_IP} github.com' >> /private/etc/hosts"
 cat /private/etc/hosts
 
 brew update
-./tests/test-utils/installs/brew-install grep gnu-sed
+"${BASE_NAME}"/brew-install grep gnu-sed
 # Coreutils and git should be already installed on OSX 7.3+ images:
 #brew install coreutils git
 # Openssl is required for installing git from source code
 # Findutils is required for `fonts` Pearl package
-./tests/test-utils/installs/brew-install openssl findutils
-
-BASE_NAME="$(dirname $0)"
+"${BASE_NAME}"/brew-install openssl findutils
 
 "${BASE_NAME}"/install-bash.sh "$TRAVIS_BASH_VERSION"
 "${BASE_NAME}"/install-zsh.sh "$TRAVIS_ZSH_VERSION"
