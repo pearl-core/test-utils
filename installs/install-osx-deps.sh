2edb2828f5743d7fa18dc3628ec935a1b49b7e4d
Filippo Squillace <squillace.filippo@gmail.com>
2020-12-11 16:18:13 +0100

    Temporarily disable brew update

--- installs/install-osx-deps.sh
+++ installs/install-osx-deps.sh
@@ -9,7 +9,11 @@ STATIC_IP="$(dig +short +tcp github.com)"
 sudo bash -c "echo '${STATIC_IP} github.com' >> /private/etc/hosts"
 cat /private/etc/hosts
 
-brew update
+# https://github.com/Homebrew/brew/issues/9420
+#git -C "/usr/local/Homebrew/Library/Taps/homebrew/homebrew-core" fetch --unshallow
+#git -C "/usr/local/Homebrew/Library/Taps/homebrew/homebrew-cask" fetch --unshallow
+#brew update
+
 "${BASE_NAME}"/brew-install grep gnu-sed
 # Coreutils and git should be already installed on OSX 7.3+ images:
 #brew install coreutils git
